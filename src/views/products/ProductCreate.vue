e0a0e1518548fa10816c5dfd1c93ebc27fd7f637
Hieu Chu <minhieuchu@gmail.com>
2022-05-05 15:40:37 +0900

    Update UI & Add model-binding for Product Create form

--- src/views/products/ProductCreate.vue
+++ src/views/products/ProductCreate.vue
@@ -1,11 +1,39 @@
 <template>
     <h2>Product create</h2>
     <div class="create-product-form">
-        <input type="text" id="product-name" name="product-name" placeholder="Product name">
-        <input type="text" id="product-price" name="product-price" placeholder="Product price">
-        <input type="text" id="owner-name" name="owner-name" placeholder="Owner name">
+        <label for="product-name">Product Name</label>
+        <input type="text" id="product-name" name="product-name" placeholder="..." v-model="product.name">
+        <label for="product-price">Product Price</label>
+        <input type="text" id="product-price" name="product-price" v-model="product.price">
+        <label for="owner-name">Owner Name</label>
+        <input type="text" id="owner-name" name="owner-name" placeholder="..." v-model="product.owner.name">
+        <div class="submit-button" @click="submitNewProduct">Create</div>
     </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+import { IProductInfo } from '@/services/interfaces/IProduct'
+
+export default defineComponent({
+    setup() {
+        const product: IProductInfo = {
+            name: "",
+            price: 0,
+            owner: {name : ""},
+            updatedAt: ""
+        }
+
+        return { product }
+    },
+    methods: {
+        submitNewProduct() {
+            console.log('Create product: ', this.product)
+        }
+    }
+})
+</script>
 
 <style scoped>
 .create-product-form {
@@ -16,5 +44,23 @@
     max-width: 300px;
     margin: 0 auto;
     margin-top: 20px;
+}
+.create-product-form > label {
+    margin-right: auto;
+}
+.create-product-form > input {
+    border: none;
+    outline: none;
+    border-bottom: 2px solid cadetblue;
+    height: 22px;
+}
+.create-product-form > .submit-button {
+    width: 40%;
+    margin: 0 auto;
+    margin-top: 30px;
+    border-radius: 4px;
+    background: cadetblue;
+    font-weight: bold;
+    color: whitesmoke;
 }
 </style>
