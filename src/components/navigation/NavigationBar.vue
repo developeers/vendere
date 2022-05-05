a6eb9e52bbd23450e482593345f830d55196f954
Hieu Chu <minhieuchu@gmail.com>
2022-05-06 08:17:30 +0900

    Implement hide / show effect for dropdown menu on navbar with DOM manipulation

--- src/components/navigation/NavigationBar.vue
+++ src/components/navigation/NavigationBar.vue
@@ -3,10 +3,43 @@
         <router-link :to="{name: 'home'}" class="app-name">Vendere</router-link>
         <input type="text" class="search-bar" placeholder="Search here">
         <div class="profile">
-            <img alt="Profile image" src="../../assets/user.png">
+            <img alt="Profile image" id="profile-icon" src="../../assets/user.png">
+            <div class="profile-menu-dropdown" id="profile-menu-dropdown">
+                <div class="dropdown-items">
+                    <router-link to="#">Orders</router-link>
+                    <router-link to="#">Settings</router-link>
+                    <router-link :to="{name: 'ProductCreate'}">Create Products</router-link>
+                </div>
+            </div>
         </div>
     </nav>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    mounted() {
+        const dropdownMenu = document.getElementById("profile-menu-dropdown")
+
+        document.getElementById("profile-icon")!.addEventListener("click", () => {
+            if (dropdownMenu!.style.display === "block") {
+                dropdownMenu!.style.display = "none"
+            } else {
+                dropdownMenu!.style.display = "block"
+            }
+        })
+        document.addEventListener("click", (event) => {
+            if (event.target instanceof Element) {
+                if (event.target.id != "profile-icon") {
+                    dropdownMenu!.style.display = "none"
+                }
+            }
+        })
+    },
+})
+</script>
+
 
 <style scoped>
 .nav-bar {
@@ -41,5 +74,30 @@
     width: 28px;
     border-radius: 50%;
     cursor: pointer;
+}
+.profile-menu-dropdown {
+    display: none;
+    background: white;
+    position: fixed;
+    top: 48px;
+    right: 0;
+}
+.dropdown-items {
+    display: flex;
+    flex-direction: column;
+}
+.dropdown-items a {
+    text-decoration: none;
+    font-style: italic;
+    font-size: 15px;
+    text-align: left;
+    color: black;
+    border: 1px solid #296495;
+    border-top: 0;
+    padding: 3px 10px;
+}
+.dropdown-items a:hover {
+    background: rgb(174, 210, 212);
+    color: whitesmoke;
 }
 </style>
