72c6a9b17729df4262b1ce4c5819bed21aa3addd
Hieu Chu <minhieuchu@gmail.com>
2022-05-15 15:21:54 +0900

    Refactor: Add helper functions to create request body when requesting firestore with query

--- src/services/utils/apiUtils.ts
+++ src/services/utils/apiUtils.ts
@@ -1,16 +1,31 @@
-import { IProductInfo } from '@/services/interfaces/IProduct'
-import { ISellerReview } from '../interfaces/ISellerReview'
-import { IUserInfo } from '../interfaces/IUser'
-import { IApiResponse, IApiResponseDocument } from '../vendereApi/vendereApiResponse/IApiResponse'
+import { IProductInfo } from "@/services/interfaces/IProduct";
+import { ISellerReview } from "../interfaces/ISellerReview";
+import { IUserInfo } from "../interfaces/IUser";
+import {
+    IApiResponse,
+    IApiResponseDocument,
+} from "../vendereApi/vendereApiResponse/IApiResponse";
 
-export const convertApiResponseProduct = (response: IApiResponse): IProductInfo => {
+export const firestoreQueryOperators = {
+    EQUAL: "EQUAL"
+}
+
+export interface IFirestoreFieldFilter {
+    fieldPath: string
+    value: string
+    op: string
+}
+
+export const convertApiResponseProduct = (
+    response: IApiResponse
+): IProductInfo => {
     const product: IProductInfo = {
         name: response.fields.name.stringValue,
         price: +response.fields.price.integerValue,
         sellerHashId: response.fields.sellerHashId.stringValue,
         updatedAt: response.updateTime,
         imageUrl: response.fields.imageUrl.stringValue,
-        hashId: response.name.split('/').slice(-1)[0]
+        hashId: response.name.split("/").slice(-1)[0],
     }
     return product
 }
@@ -21,19 +36,59 @@ export const convertApiResponseUser = (response: IApiResponse): IUserInfo => {
         averageReview: +response.fields.averageReview.doubleValue,
         numOfReviews: +response.fields.numOfReviews.integerValue,
         imageUrl: response.fields.imageUrl.stringValue,
-        hashId: response.name.split('/').slice(-1)[0]
+        hashId: response.name.split("/").slice(-1)[0],
     }
     return user
 }
 
-export const convertApiResponseSellerReviews = (response: Array<IApiResponseDocument>): Array<ISellerReview> => {
-    const sellerReviews = response.map((review: IApiResponseDocument): ISellerReview => {
-        const sellerReview: ISellerReview = {
-            targetSellerHashId: review.document.fields.targetSellerHashId.stringValue,
-            reviewUserHashId: review.document.fields.reviewUserHashId.stringValue,
-            content: review.document.fields.content.stringValue
+export const convertApiResponseSellerReviews = (
+    response: Array<IApiResponseDocument>
+): Array<ISellerReview> => {
+    const sellerReviews = response.map(
+        (review: IApiResponseDocument): ISellerReview => {
+            const sellerReview: ISellerReview = {
+                targetSellerHashId:
+                    review.document.fields.targetSellerHashId.stringValue,
+                reviewUserHashId: review.document.fields.reviewUserHashId.stringValue,
+                content: review.document.fields.content.stringValue,
+            };
+            return sellerReview
         }
-        return sellerReview
-    })
+    )
     return sellerReviews
+}
+
+const createFirestoreFieldFilter = (fieldFilter: IFirestoreFieldFilter) => {
+    return {
+        fieldFilter: {
+            field: {
+                fieldPath: fieldFilter.fieldPath,
+            },
+            op: fieldFilter.op,
+            value: {
+                stringValue: fieldFilter.value,
+            },
+        },
+    }
+}
+
+type FirestoreFormattedFieldFilter = ReturnType<typeof createFirestoreFieldFilter>
+
+export const createFirestoreRequestBody = (collectionName: string, fieldFilters: Array<IFirestoreFieldFilter>) => {
+    const firestoreRequestBody = {
+        structuredQuery: {
+            from: [{ collectionId: collectionName }],
+            where: {
+                compositeFilter: {
+                    filters: [] as Array<FirestoreFormattedFieldFilter>,
+                    op: "AND",
+                },
+            },
+        },
+    }
+    fieldFilters.forEach(fieldFilter => {
+        const formattedFieldFilter = createFirestoreFieldFilter(fieldFilter)
+        firestoreRequestBody.structuredQuery.where.compositeFilter.filters.push(formattedFieldFilter)
+    })
+    return firestoreRequestBody
 }
--- src/services/vendereApi/VendereApiUser.ts
+++ src/services/vendereApi/VendereApiUser.ts
@@ -1,37 +1,29 @@
-import { VendereApiInstance } from './VendereApiBase'
+import { VendereApiInstance, VendereQueryApiInstance } from "./VendereApiBase"
 import { IUserInfo } from "../interfaces/IUser"
-import { ISellerReview } from '../interfaces/ISellerReview'
-import { convertApiResponseUser, convertApiResponseSellerReviews } from '../utils/apiUtils'
-import axios from 'axios'
+import { ISellerReview } from "../interfaces/ISellerReview"
+import {
+    convertApiResponseUser,
+    convertApiResponseSellerReviews,
+    IFirestoreFieldFilter,
+    firestoreQueryOperators,
+    createFirestoreRequestBody,
+} from "../utils/apiUtils"
 
 export const getUserByHashId = async (hashId: string): Promise<IUserInfo> => {
     const res = await VendereApiInstance.get(`users/${hashId}`)
     return convertApiResponseUser(res.data)
 }
 
-export const getSellerReviews = async (sellerHashId: string): Promise<Array<ISellerReview>> => {
-    const res = await axios.post('https://firestore.googleapis.com/v1/projects/vendere-23f76/databases/(default)/documents:runQuery', {
-        structuredQuery: {
-            from: [
-                { collectionId: 'seller_review' }
-            ],
-            where: {
-                compositeFilter: {
-                    filters: [{
-                        fieldFilter: {
-                            field: {
-                                fieldPath: 'targetSellerHashId'
-                            },
-                            op: 'EQUAL',
-                            value: {
-                                stringValue: sellerHashId
-                            }
-                        }
-                    }],
-                    op: 'AND'
-                }
-            }
-        }
-    })
+export const getSellerReviews = async (
+    sellerHashId: string
+): Promise<Array<ISellerReview>> => {
+    const fieldFilter: IFirestoreFieldFilter = {
+        fieldPath: "targetSellerHashId",
+        value: sellerHashId,
+        op: firestoreQueryOperators.EQUAL
+    }
+    const sellerReviewsRequestBody = createFirestoreRequestBody("seller_review", [fieldFilter])
+    const res = await VendereQueryApiInstance.post("", sellerReviewsRequestBody)
+
     return convertApiResponseSellerReviews(res.data)
 }
