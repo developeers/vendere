cd1df64b83ffc6083d441a43fa482f0db9936f05
Hieu Chu <minhieuchu@gmail.com>
2022-06-17 17:54:20 +0900

    Add authToken to global store (user module)

--- src/store/modules/User.ts
+++ src/store/modules/User.ts
@@ -1,12 +1,13 @@
 import store from '../index'
 import { IUserInfo } from '@/services/interfaces/IUser'
+import localStorageUtils from '@/services/utils/localStorageUtils'
 
 export interface UserState {
-    name: string
-    hashId?: string
-    averageReview: number
-    numOfReviews: number
-    imageUrl?: string
+  name: string
+  hashId?: string
+  averageReview: number
+  numOfReviews: number
+  imageUrl?: string
 }
 
 import { Module, VuexModule, Mutation, getModule } from 'vuex-module-decorators'
@@ -14,14 +15,29 @@ import { Module, VuexModule, Mutation, getModule } from 'vuex-module-decorators'
 @Module({ dynamic: true, store: store, name: 'UserModule' })
 class UserModule extends VuexModule {
   private _userInfoList: Array<IUserInfo> = []
+  private _authTokenLocalStorageKey = "auth_token";
+  private _authToken: string = localStorageUtils.getItem(this._authTokenLocalStorageKey) ?? "";
 
   @Mutation
   addUserInfo(userInfo: IUserInfo) {
     this._userInfoList.push(userInfo)
   }
+  @Mutation
+  setAuthToken(authToken: string | null) {
+    if (authToken) {
+      localStorageUtils.setItem(this._authTokenLocalStorageKey, authToken);
+      this._authToken = authToken;
+    } else {
+      localStorageUtils.removeItem(this._authTokenLocalStorageKey);
+      this._authToken = "";
+    }
+  }
 
   get userInfo() {
     return (hashId: string) => this._userInfoList.find((userInfo) => userInfo.hashId == hashId)
+  }
+  get authToken() {
+    return this._authToken;
   }
 }
 
