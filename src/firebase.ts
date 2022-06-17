a9c7d8172bcd4e97361ae6a42183502fa6856559
Hieu Chu <minhieuchu@gmail.com>
2022-06-17 11:41:15 +0900

    Install firebase package & Setup firebase object app

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC5FRiW7CiNiSu_Qpufsnsx-3yYO_hr8Ls",
+    authDomain: "vendere-23f76.firebaseapp.com",
+    projectId: "vendere-23f76",
+    storageBucket: "vendere-23f76.appspot.com",
+    messagingSenderId: "483241483648",
+    appId: "1:483241483648:web:496f2601d1f542a4ee3565"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
