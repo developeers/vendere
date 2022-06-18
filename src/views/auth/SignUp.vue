24b168acd595ff7b555d917c659b16803c6a6fc5
Hieu Chu <minhieuchu@gmail.com>
2022-06-18 19:08:01 +0900

    Add validation to user signup form

--- src/views/auth/SignUp.vue
+++ src/views/auth/SignUp.vue
@@ -2,17 +2,37 @@
   <div class="signup-form">
     <div class="email-input">
       <label for="email">Email</label>
-      <input type="email" name="email" placeholder="Enter your email" />
+      <input
+        type="email"
+        name="email"
+        placeholder="Enter your email"
+        @mousedown="resetInputFieldState"
+      />
+      <span class="warning-message">* This field is required.</span>
     </div>
     <div class="username-input">
       <label for="username">Username</label>
-      <input type="text" name="username" placeholder="Create a username" />
+      <input
+        type="text"
+        name="username"
+        placeholder="Create a username"
+        @mousedown="resetInputFieldState"
+      />
+      <span class="warning-message">* This field is required.</span>
     </div>
     <div class="password-input">
       <label for="password">Password</label>
-      <input type="password" name="password" placeholder="Create a password" />
+      <input
+        type="password"
+        name="password"
+        placeholder="Create a password"
+        @mousedown="resetInputFieldState"
+      />
+      <span class="warning-message">* This field is required.</span>
     </div>
-    <button type="text" class="signup-button">Sign up</button>
+    <button type="text" class="signup-button" @click="signUpFirebaseUser">
+      Sign up
+    </button>
   </div>
 </template>
 
@@ -25,16 +45,38 @@ import {
 } from "@firebase/auth";
 
 export default defineComponent({
-  mounted() {
-    const auth = getAuth();
-    const submitButton = document.querySelector("button.signup-button");
-    submitButton?.addEventListener("click", () => {
+  methods: {
+    signUpFirebaseUser() {
+      const auth = getAuth();
       const emailInput = document.querySelector(
         `input[type="email"]`
       ) as HTMLInputElement;
       const passwordInput = document.querySelector(
         `input[type="password"]`
       ) as HTMLInputElement;
+      const usernameInput = document.querySelector(
+        `input[name="username"]`
+      ) as HTMLInputElement;
+
+      // Order matters as only the first empty element can be focused
+      const inputFields = [passwordInput, usernameInput, emailInput];
+
+      const outlineInputElement = (element: HTMLInputElement): void => {
+        element.classList.add("warning");
+        element.focus();
+      };
+
+      let isEmpty = false;
+      inputFields.forEach((inputField) => {
+        if (!inputField.value) {
+          isEmpty = true;
+          outlineInputElement(inputField);
+        }
+      });
+
+      if (isEmpty) {
+        return;
+      }
 
       createUserWithEmailAndPassword(
         auth,
@@ -42,9 +84,6 @@ export default defineComponent({
         passwordInput.value
       )
         .then(() => {
-          const usernameInput = document.querySelector(
-            `input[name="username"]`
-          ) as HTMLInputElement;
           updateProfile(auth.currentUser!, {
             displayName: usernameInput.value,
           });
@@ -52,7 +91,11 @@ export default defineComponent({
         .catch((error) => {
           console.log("Error signing up user: ", error);
         });
-    });
+    },
+    resetInputFieldState(event: Event) {
+      const inputField = event.target as HTMLInputElement;
+      inputField.classList.remove("warning");
+    },
   },
 });
 </script>
@@ -73,13 +116,28 @@ label {
   margin-bottom: 5px;
 }
 input {
-  outline: none;
+  outline-color: cadetblue;
   height: 30px;
   font-size: 15px;
   color: #3d3d3d;
   padding-left: 8px;
   border: 1px solid #ffaee1;
   border-radius: 4px;
+}
+span.warning-message {
+  display: none;
+}
+input.warning {
+  box-shadow: 0 0 5px orange;
+  border-color: orange;
+  outline: none;
+}
+input.warning + span.warning-message {
+  display: initial;
+  font-size: 14px;
+  color: orange;
+  text-align: left;
+  margin-top: 5px;
 }
 .email-input,
 .password-input,
