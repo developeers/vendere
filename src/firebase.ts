ebc127600f50b42763b817fb5bc19445c833c512
Hieu Chu <minhieuchu@gmail.com>
2022-06-25 15:05:43 +0900

    Request useInfo from Firestore and save date to Vuex when auth state changes

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,6 +2,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
+import { getUserByUID } from './services/vendereApi/VendereApiUser';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import UserModule from './store/modules/User';
@@ -26,5 +27,6 @@ onAuthStateChanged(auth, (user) => {
         // User is signed in, see docs for a list of available properties
         // https://firebase.google.com/docs/reference/js/firebase.User
         UserModule.setFirebaseUser(user);
+        getUserByUID(user.uid).then(userInfo => UserModule.setLoginUser(userInfo))
     }
 });
