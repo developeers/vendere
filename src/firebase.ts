b37cc098743464538cd0cbed20229e5b5867a222
Hieu Chu <minhieuchu@gmail.com>
2022-06-26 14:38:06 +0900

    Add backup Firebase project settings

--- src/firebase.ts
+++ src/firebase.ts
@@ -8,13 +8,23 @@ import { getUserByUID } from './services/vendereApi/VendereApiUser';
 import UserModule from './store/modules/User';
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//     apiKey: "AIzaSyC5FRiW7CiNiSu_Qpufsnsx-3yYO_hr8Ls",
+//     authDomain: "vendere-23f76.firebaseapp.com",
+//     projectId: "vendere-23f76",
+//     storageBucket: "vendere-23f76.appspot.com",
+//     messagingSenderId: "483241483648",
+//     appId: "1:483241483648:web:496f2601d1f542a4ee3565"
+// };
+
+// Backup Firebase project
 const firebaseConfig = {
-    apiKey: "AIzaSyC5FRiW7CiNiSu_Qpufsnsx-3yYO_hr8Ls",
-    authDomain: "vendere-23f76.firebaseapp.com",
-    projectId: "vendere-23f76",
-    storageBucket: "vendere-23f76.appspot.com",
-    messagingSenderId: "483241483648",
-    appId: "1:483241483648:web:496f2601d1f542a4ee3565"
+    apiKey: "AIzaSyBQBxjN7cRUwBu23Xd94XQWlbJoTCv-zJI",
+    authDomain: "vendere-d6f89.firebaseapp.com",
+    projectId: "vendere-d6f89",
+    storageBucket: "vendere-d6f89.appspot.com",
+    messagingSenderId: "1060212968640",
+    appId: "1:1060212968640:web:48090c96802bd9001ff125"
 };
 
 // Initialize Firebase
