9265443aa0f7e1311a03e4678ae65ca1275021a1
Hieu Chu <minhieuchu@gmail.com>
2022-07-02 18:38:24 +0900

    Preview image when upload in UploadImage component

--- src/components/utils/UploadImage.vue
+++ src/components/utils/UploadImage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="drop-area" ref="dropArea">
+  <div :class="dropAreaClasses" ref="dropArea">
     <i class="fa fa-camera"></i>
     <div class="drop-area-text">
       <p>Drop image here</p>
@@ -14,10 +14,33 @@
 import { defineComponent } from "vue";
 
 export default defineComponent({
+  data() {
+    return {
+      numUploadedImages: 0,
+    };
+  },
+  computed: {
+    dropAreaClasses() {
+      if (this.numUploadedImages) {
+        return "drop-area drop-area-with-image";
+      }
+      return "drop-area";
+    },
+  },
   methods: {
     customPreventDefault(event: Event) {
       event.preventDefault();
       event.stopPropagation();
+    },
+    previewImage(image: File) {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(image);
+      fileReader.onloadend = () => {
+        const img = document.createElement("img");
+        img.src = fileReader.result as string;
+        (this.$refs.dropArea as HTMLElement).appendChild(img);
+        this.numUploadedImages += 1;
+      };
     },
   },
   created() {
@@ -41,7 +64,17 @@ export default defineComponent({
     (this.$refs.dropArea as HTMLElement).addEventListener(
       "drop",
       (event: DragEvent) => {
-        console.log(event.dataTransfer?.files);
+        if (!event.dataTransfer) {
+          return;
+        }
+        const draggedFiles = [...event.dataTransfer.files];
+        if (!draggedFiles.length) {
+          return;
+        }
+        const draggedImages = draggedFiles.filter((file: File) => {
+          return file.type.startsWith("image");
+        });
+        draggedImages.forEach((image) => this.previewImage(image));
       }
     );
   },
@@ -65,6 +98,11 @@ export default defineComponent({
   margin-bottom: 10px;
   border-radius: 4px;
   border: 1px dashed grey;
+}
+.drop-area.drop-area-with-image {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 15px;
 }
 .drop-area > .fa-camera {
   font-size: 30px;
@@ -99,5 +137,16 @@ export default defineComponent({
 }
 input[type="file"] {
   display: none;
+}
+</style>
+
+<style>
+/* Image elements are created based on user actions, and those elements
+will not be given attributes that bind them to scoped CSS. 
+Therefore, it's neccessary to style image elements in non-scoped CSS.
+*/
+.drop-area img {
+  width: 40px;
+  height: 36px;
 }
 </style>
