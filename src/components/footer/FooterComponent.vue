4ae20271a7f659a64aff72343458119abd50d024
Hieu Chu <minhieuchu@gmail.com>
2022-07-08 10:45:47 +0900

    Add FooterComponent

---
+++ src/components/footer/FooterComponent.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="footer-container">
+    <div class="footer-container-column">
+      <a href="#">SHOP</a>
+      <a href="#">Categories</a>
+      <a href="#">Brands</a>
+      <a href="#">Keywords</a>
+    </div>
+    <div class="footer-container-column">
+      <a href="#">SERVICES</a>
+      <a href="#">Buy Now, Pay Later</a>
+      <a href="#">Shipping</a>
+      <a href="#">Instant Pay</a>
+      <a href="#">Sell from Home</a>
+    </div>
+    <div class="footer-container-column">
+      <a href="#">SUPPORT</a>
+      <a href="#">Contact us</a>
+      <a href="#">Help Center</a>
+      <a href="#">Seller Protection</a>
+      <a href="#">Buyer Protection</a>
+    </div>
+    <div class="footer-container-column">
+      <a href="#">COMPANY</a>
+      <a href="#">About us</a>
+      <a href="#">Blog</a>
+      <a href="#">Career</a>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
+
+
+<style scoped>
+.footer-container {
+  display: flex;
+  justify-content: space-around;
+  padding: 20px;
+  background: whitesmoke;
+  border-top: 2px solid mediumvioletred;
+}
+.footer-container-column {
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+}
+.footer-container-column > a {
+  text-decoration: none;
+  color: mediumvioletred;
+}
+.footer-container-column > a:first-child {
+  font-weight: bold;
+  font-size: 18px;
+}
+</style>
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -3,15 +3,18 @@
   <div class="body-container">
     <router-view />
   </div>
+  <footer-component></footer-component>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import NavigationBar from "@/components/navigation/NavigationBar.vue";
+import FooterComponent from "@/components/footer/FooterComponent.vue";
 
 export default defineComponent({
   components: {
     NavigationBar,
+    FooterComponent,
   },
 });
 </script>
