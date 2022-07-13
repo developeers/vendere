cf381bc20447962c399936e85ecca9b60666c504
Hieu Chu <minhieuchu@gmail.com>
2022-07-13 20:56:59 +0900

    Add OrderItem component

--- src/views/order/OrderHistory.vue
+++ src/views/order/OrderHistory.vue
@@ -1,18 +1,31 @@
 <template>
-  <div class="container">
+  <div class="order-history-container">
     <h2>Your Orders</h2>
+    <order-item v-for="index in 10" :key="index"></order-item>
   </div>
 </template>
 
+<script lang="ts">
+import { defineComponent } from "vue";
+import OrderItem from "./OrderItem.vue";
+
+export default defineComponent({
+  components: {
+    OrderItem,
+  },
+});
+</script>
+
+
 <style scoped>
-.container {
+.order-history-container {
   width: 80%;
   max-width: 900px;
   min-width: 600px;
   text-align: left;
   margin: 0 auto;
 }
-.container > h2 {
+.order-history-container > h2 {
   color: mediumvioletred;
   padding-bottom: 18px;
   border-bottom: 1px solid #dadada;
---
+++ src/views/order/OrderItem.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="order-item-container">
+    <div class="order-meta-data">
+      <div class="order-date">ORDER PLACED</div>
+      <div class="order-total">TOTAL</div>
+      <div class="order-shipping-destination"></div>
+      <div class="order-id">ORDER #123</div>
+    </div>
+    <div class="order-detail-container">
+      <div class="order-item-detail">
+        <img src="@/assets/app_logo.png" alt="" />
+        <div class="order-item-name">
+          <p>Item Name</p>
+          <div class="buttons-container">
+            <button class="buy-again">Buy Again</button>
+            <button class="view-item">View your item</button>
+          </div>
+        </div>
+      </div>
+      <div class="order-operation">
+        <button>Return Item</button>
+        <button>Write a Review</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.order-item-container {
+  border: 1px solid #cecece;
+  border-radius: 5px;
+  margin-top: 30px;
+}
+.order-meta-data {
+  background: #f1f1f1;
+  display: flex;
+  gap: 25px;
+  padding: 10px 15px;
+}
+.order-id {
+  margin-left: auto;
+}
+.order-detail-container {
+  display: flex;
+  justify-content: space-between;
+  padding: 15px;
+}
+.order-item-detail {
+  display: flex;
+  gap: 15px;
+}
+.order-item-detail > img {
+  width: 80px;
+  height: 76px;
+}
+.order-item-name {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+.buttons-container {
+  display: flex;
+  gap: 5px;
+}
+.buttons-container > button {
+  width: 150px;
+  border-radius: 7px;
+  padding: 7px 10px;
+  box-sizing: border-box;
+  cursor: pointer;
+  border: none;
+}
+.buttons-container > button:hover {
+  opacity: 0.8;
+}
+.buttons-container > button:first-child {
+  background: cadetblue;
+  color: whitesmoke;
+}
+.order-operation {
+  display: flex;
+  flex-direction: column;
+  gap: 7px;
+}
+.order-operation > button {
+  width: 180px;
+  padding: 7px 15px;
+  background: white;
+  border: 1px solid rgb(182, 182, 182);
+  box-shadow: 0 0 3px silver;
+  border-radius: 5px;
+  box-sizing: border-box;
+  cursor: pointer;
+}
+.order-operation > button:hover {
+  background: #f6f6f6;
+}
+</style>
