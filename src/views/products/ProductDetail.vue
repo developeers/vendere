63f866ad1e6188d3218443d4c123864885186a7e
Hieu Chu <minhieuchu@gmail.com>
2022-07-17 08:12:03 +0900

    Update Product image container height after rendering the first time

--- src/views/products/ProductDetail.vue
+++ src/views/products/ProductDetail.vue
@@ -105,6 +105,9 @@ export default defineComponent({
           UserModule.addUserInfo(sellerInfo);
           this.sellerInfo = sellerInfo;
           this.isLoading = false;
+          setTimeout(() => {
+            this.updateContainerHeight();
+          }, 0);
         });
       }
     });
@@ -133,7 +136,6 @@ export default defineComponent({
     },
   },
   mounted() {
-    this.updateContainerHeight();
     window.addEventListener("resize", this.updateContainerHeight);
   },
   beforeUnmount() {
