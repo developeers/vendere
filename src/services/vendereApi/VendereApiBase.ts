284ab79bb88d1ace7a8617156bfac8da88b6de16
Hieu Chu <minhieuchu@gmail.com>
2022-07-24 15:30:47 +0900

    Use URL string directly instead of env variables in Axios clients

--- src/services/vendereApi/VendereApiBase.ts
+++ src/services/vendereApi/VendereApiBase.ts
@@ -6,66 +6,79 @@ import UserModule from '@/store/modules/User';
 import httpStatusCodes from '../utils/httpStatusCodes';
 
 const config: AxiosRequestConfig = {
-    baseURL: process.env.VUE_APP_API_SERVER_URL
-}
+  baseURL:
+    "https://firestore.googleapis.com/v1/projects/vendere-23f76/databases/(default)/documents/",
+};
 const queryApiConfig: AxiosRequestConfig = {
-    baseURL: process.env.VUE_APP_API_SERVER_QUERY_URL
-}
+  baseURL:
+    "https://firestore.googleapis.com/v1/projects/vendere-23f76/databases/(default)/documents:runQuery",
+};
 
 interface RefreshAccessTokenResponse {
-    id_token: string
-    refresh_token: string
+  id_token: string;
+  refresh_token: string;
 }
 
 interface RefreshAccessTokenApiResponse {
-    data: RefreshAccessTokenResponse
+  data: RefreshAccessTokenResponse;
 }
 
-const interceptAxiosRequest = async (config: AxiosRequestConfig): Promise<AxiosRequestConfig> => {
-    const accessToken = UserModule.accessToken
-    if (accessToken) {
-        return {
-            ...config,
-            headers: {
-                ...config.headers,
-                Authorization: `Bearer ${accessToken}`
-            }
-        }
-    }
-    return config;
-}
+const interceptAxiosRequest = async (
+  config: AxiosRequestConfig
+): Promise<AxiosRequestConfig> => {
+  const accessToken = UserModule.accessToken;
+  if (accessToken) {
+    return {
+      ...config,
+      headers: {
+        ...config.headers,
+        Authorization: `Bearer ${accessToken}`,
+      },
+    };
+  }
+  return config;
+};
 
 const interceptAxiosResponseError = async (error: AxiosError) => {
-    if (error.response?.status == httpStatusCodes.UNAUTHORIZED) {
-        // Refresh access token
-        const refreshToken = UserModule.refreshToken
-        const originalRequest = error.config
-        await axios.post<any, RefreshAccessTokenApiResponse>(refreshAccessTokenApiUrl, {
-            grant_type: "refresh_token",
-            refresh_token: refreshToken
-        }).then(res => {
-            UserModule.setRefreshToken(res.data.refresh_token)
-            UserModule.setAccessToken(res.data.id_token)
-        }).catch(() => {
-            // To-do: Handle when a new access token can not be issued
-        })
-        const newAccessToken = UserModule.accessToken
-        originalRequest.headers = {
-            ...originalRequest.headers,
-            Authorization: `Bearer ${newAccessToken}`
-        }
-        return await axios(originalRequest)
-    }
-    return error
-}
+  if (error.response?.status == httpStatusCodes.UNAUTHORIZED) {
+    // Refresh access token
+    const refreshToken = UserModule.refreshToken;
+    const originalRequest = error.config;
+    await axios
+      .post<any, RefreshAccessTokenApiResponse>(refreshAccessTokenApiUrl, {
+        grant_type: "refresh_token",
+        refresh_token: refreshToken,
+      })
+      .then((res) => {
+        UserModule.setRefreshToken(res.data.refresh_token);
+        UserModule.setAccessToken(res.data.id_token);
+      })
+      .catch(() => {
+        // To-do: Handle when a new access token can not be issued
+      });
+    const newAccessToken = UserModule.accessToken;
+    originalRequest.headers = {
+      ...originalRequest.headers,
+      Authorization: `Bearer ${newAccessToken}`,
+    };
+    return await axios(originalRequest);
+  }
+  return error;
+};
 
-const VendereApiInstance: AxiosInstance = axios.create(config)
-const VendereQueryApiInstance: AxiosInstance = axios.create(queryApiConfig)
+const VendereApiInstance: AxiosInstance = axios.create(config);
+const VendereQueryApiInstance: AxiosInstance = axios.create(queryApiConfig);
 
 VendereApiInstance.interceptors.request.use(interceptAxiosRequest);
 VendereQueryApiInstance.interceptors.request.use(interceptAxiosRequest);
 
-VendereApiInstance.interceptors.response.use(undefined, interceptAxiosResponseError)
-VendereQueryApiInstance.interceptors.response.use(undefined, interceptAxiosResponseError)
+VendereApiInstance.interceptors.response.use(
+  undefined,
+  interceptAxiosResponseError
+);
+VendereQueryApiInstance.interceptors.response.use(
+  undefined,
+  interceptAxiosResponseError
+);
 
 export { VendereApiInstance, VendereQueryApiInstance };
